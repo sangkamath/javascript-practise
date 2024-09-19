e31f1b2bf885b591a49d6e7127462c8e1556ef87
sangkamath <twinkle_kamath@yahoo.com>
2024-09-19 19:27:16 +0530

    Added new cases

---
+++ listpagination.js
@@ -0,0 +1,1007 @@
+const dataSet = [
+    {
+      "_id": 7703354,
+      "name": "Alison Joseph"
+    },
+    {
+      "_id": 5072488,
+      "name": "Chaney Mccall"
+    },
+    {
+      "_id": 2588577,
+      "name": "Mason Spencer"
+    },
+    {
+      "_id": 2180840,
+      "name": "Mary Orr"
+    },
+    {
+      "_id": 3396494,
+      "name": "Mercer Brooks"
+    },
+    {
+      "_id": 9759634,
+      "name": "Houston James"
+    },
+    {
+      "_id": 1789304,
+      "name": "Naomi Atkinson"
+    },
+    {
+      "_id": 3384914,
+      "name": "Kristy Nieves"
+    },
+    {
+      "_id": 6821119,
+      "name": "Holden Morton"
+    },
+    {
+      "_id": 567606,
+      "name": "Mae Ware"
+    },
+    {
+      "_id": 3659135,
+      "name": "Milagros Fleming"
+    },
+    {
+      "_id": 7208150,
+      "name": "Beard Nicholson"
+    },
+    {
+      "_id": 2371035,
+      "name": "Clara Vargas"
+    },
+    {
+      "_id": 1024397,
+      "name": "Garrison Wise"
+    },
+    {
+      "_id": 9449750,
+      "name": "Debra Owens"
+    },
+    {
+      "_id": 6176153,
+      "name": "Manning Mcgee"
+    },
+    {
+      "_id": 9295654,
+      "name": "Osborne Foley"
+    },
+    {
+      "_id": 4150571,
+      "name": "Clarice Watts"
+    },
+    {
+      "_id": 2350520,
+      "name": "Hurley Francis"
+    },
+    {
+      "_id": 2382635,
+      "name": "Tyler Rich"
+    },
+    {
+      "_id": 9884559,
+      "name": "Lawrence Raymond"
+    },
+    {
+      "_id": 5697484,
+      "name": "Thomas Rios"
+    },
+    {
+      "_id": 6879423,
+      "name": "Rivers Richards"
+    },
+    {
+      "_id": 2421764,
+      "name": "Gray Boyle"
+    },
+    {
+      "_id": 7507181,
+      "name": "Kristine Ray"
+    },
+    {
+      "_id": 3097583,
+      "name": "Abby Sears"
+    },
+    {
+      "_id": 8856109,
+      "name": "Brittney Hoffman"
+    },
+    {
+      "_id": 6838244,
+      "name": "Williamson Anderson"
+    },
+    {
+      "_id": 7139227,
+      "name": "Mclean Frederick"
+    },
+    {
+      "_id": 918371,
+      "name": "Davis Rollins"
+    },
+    {
+      "_id": 8839168,
+      "name": "Diann Carlson"
+    },
+    {
+      "_id": 7640801,
+      "name": "Ava Ramos"
+    },
+    {
+      "_id": 3777045,
+      "name": "Butler Simmons"
+    },
+    {
+      "_id": 1455581,
+      "name": "Moreno Parks"
+    },
+    {
+      "_id": 7245665,
+      "name": "Cash Klein"
+    },
+    {
+      "_id": 7658070,
+      "name": "Greta Franklin"
+    },
+    {
+      "_id": 468854,
+      "name": "Christina Reeves"
+    },
+    {
+      "_id": 7014648,
+      "name": "Socorro Lawrence"
+    },
+    {
+      "_id": 5548463,
+      "name": "Alexander Pratt"
+    },
+    {
+      "_id": 9361464,
+      "name": "Michael Browning"
+    },
+    {
+      "_id": 2565703,
+      "name": "Parrish Lindsay"
+    },
+    {
+      "_id": 4752132,
+      "name": "Bryant Mcdonald"
+    },
+    {
+      "_id": 8839902,
+      "name": "Deidre Noel"
+    },
+    {
+      "_id": 8945959,
+      "name": "Imelda Moran"
+    },
+    {
+      "_id": 2391511,
+      "name": "Jaclyn Riddle"
+    },
+    {
+      "_id": 3301944,
+      "name": "Tanya Harrell"
+    },
+    {
+      "_id": 5768451,
+      "name": "Gonzales Woodward"
+    },
+    {
+      "_id": 8595547,
+      "name": "Tyson Dawson"
+    },
+    {
+      "_id": 6887064,
+      "name": "Randolph Walton"
+    },
+    {
+      "_id": 2775625,
+      "name": "Lilly Bowen"
+    },
+    {
+      "_id": 4518220,
+      "name": "Brock Baldwin"
+    },
+    {
+      "_id": 4725416,
+      "name": "Autumn Austin"
+    },
+    {
+      "_id": 5724924,
+      "name": "Nelson Farrell"
+    },
+    {
+      "_id": 8570072,
+      "name": "Samantha Walter"
+    },
+    {
+      "_id": 573701,
+      "name": "Roslyn Moon"
+    },
+    {
+      "_id": 4103341,
+      "name": "Bean Webb"
+    },
+    {
+      "_id": 5031376,
+      "name": "English Jennings"
+    },
+    {
+      "_id": 7948474,
+      "name": "Kris Guzman"
+    },
+    {
+      "_id": 1068677,
+      "name": "Mariana French"
+    },
+    {
+      "_id": 7045507,
+      "name": "Muriel Emerson"
+    },
+    {
+      "_id": 1913840,
+      "name": "Clements Freeman"
+    },
+    {
+      "_id": 7590311,
+      "name": "Murphy Mayo"
+    },
+    {
+      "_id": 2071861,
+      "name": "Erickson Coleman"
+    },
+    {
+      "_id": 7772397,
+      "name": "Nixon Everett"
+    },
+    {
+      "_id": 5015791,
+      "name": "Sheila Bartlett"
+    },
+    {
+      "_id": 633904,
+      "name": "Oconnor Craig"
+    },
+    {
+      "_id": 8725400,
+      "name": "Melba Carter"
+    },
+    {
+      "_id": 4627541,
+      "name": "Roberts Fitzpatrick"
+    },
+    {
+      "_id": 8595629,
+      "name": "Herman Bowman"
+    },
+    {
+      "_id": 7449488,
+      "name": "Holcomb Mclean"
+    },
+    {
+      "_id": 6760512,
+      "name": "Barlow Simon"
+    },
+    {
+      "_id": 5803601,
+      "name": "Rosalind Wilkinson"
+    },
+    {
+      "_id": 7906613,
+      "name": "Louisa Rosa"
+    },
+    {
+      "_id": 9688666,
+      "name": "Katrina Oconnor"
+    },
+    {
+      "_id": 1332765,
+      "name": "Mcmillan Santana"
+    },
+    {
+      "_id": 3188681,
+      "name": "Gloria Holmes"
+    },
+    {
+      "_id": 6819871,
+      "name": "Pennington Roach"
+    },
+    {
+      "_id": 7652241,
+      "name": "Lara Carroll"
+    },
+    {
+      "_id": 5804374,
+      "name": "Leah Woods"
+    },
+    {
+      "_id": 9746845,
+      "name": "Fuentes Stanley"
+    },
+    {
+      "_id": 233385,
+      "name": "Kelsey Sweeney"
+    },
+    {
+      "_id": 8645326,
+      "name": "Morrow Bond"
+    },
+    {
+      "_id": 1125493,
+      "name": "Mccullough Hyde"
+    },
+    {
+      "_id": 6438416,
+      "name": "Short Acevedo"
+    },
+    {
+      "_id": 7869460,
+      "name": "Campos Rodriguez"
+    },
+    {
+      "_id": 7007288,
+      "name": "Mayer Sampson"
+    },
+    {
+      "_id": 964701,
+      "name": "Wynn Lott"
+    },
+    {
+      "_id": 2053442,
+      "name": "Colette Garrison"
+    },
+    {
+      "_id": 94649,
+      "name": "Denise Hogan"
+    },
+    {
+      "_id": 8665584,
+      "name": "Freida Kelley"
+    },
+    {
+      "_id": 8385024,
+      "name": "Owens Keller"
+    },
+    {
+      "_id": 5516670,
+      "name": "Sylvia Mckee"
+    },
+    {
+      "_id": 1149211,
+      "name": "Travis Adkins"
+    },
+    {
+      "_id": 5658839,
+      "name": "Anthony Joyner"
+    },
+    {
+      "_id": 7275411,
+      "name": "Audrey Flores"
+    },
+    {
+      "_id": 4222175,
+      "name": "Lorie Heath"
+    },
+    {
+      "_id": 2443939,
+      "name": "Goodwin Washington"
+    },
+    {
+      "_id": 1105469,
+      "name": "Genevieve Pope"
+    },
+    {
+      "_id": 5534944,
+      "name": "Valdez Arnold"
+    },
+    {
+      "_id": 883632,
+      "name": "Nora Turner"
+    },
+    {
+      "_id": 9540968,
+      "name": "Burch Atkins"
+    },
+    {
+      "_id": 7193124,
+      "name": "Morse Cleveland"
+    },
+    {
+      "_id": 9503559,
+      "name": "Collier Carey"
+    },
+    {
+      "_id": 1499631,
+      "name": "Douglas Jenkins"
+    },
+    {
+      "_id": 2454693,
+      "name": "Candace Hall"
+    },
+    {
+      "_id": 7635269,
+      "name": "Sharron Little"
+    },
+    {
+      "_id": 1012358,
+      "name": "Dixon Oneal"
+    },
+    {
+      "_id": 417728,
+      "name": "Minnie Ayers"
+    },
+    {
+      "_id": 5697271,
+      "name": "Ball Bender"
+    },
+    {
+      "_id": 4870002,
+      "name": "Lynette Fields"
+    },
+    {
+      "_id": 7443223,
+      "name": "Alyson Hawkins"
+    },
+    {
+      "_id": 9976836,
+      "name": "Vargas Coffey"
+    },
+    {
+      "_id": 7198267,
+      "name": "Arline Crawford"
+    },
+    {
+      "_id": 2568534,
+      "name": "Lydia Hopper"
+    },
+    {
+      "_id": 9029706,
+      "name": "Blair Cooper"
+    },
+    {
+      "_id": 1661416,
+      "name": "Geraldine Conner"
+    },
+    {
+      "_id": 3139439,
+      "name": "Daniel Short"
+    },
+    {
+      "_id": 892777,
+      "name": "Palmer Hensley"
+    },
+    {
+      "_id": 3005927,
+      "name": "Collins Copeland"
+    },
+    {
+      "_id": 7782702,
+      "name": "Caitlin Walsh"
+    },
+    {
+      "_id": 9930562,
+      "name": "Mcguire Christensen"
+    },
+    {
+      "_id": 9293307,
+      "name": "Irwin Bryan"
+    },
+    {
+      "_id": 9015020,
+      "name": "Marietta Pace"
+    },
+    {
+      "_id": 6076667,
+      "name": "Celia Reynolds"
+    },
+    {
+      "_id": 8015623,
+      "name": "Middleton Franco"
+    },
+    {
+      "_id": 2028985,
+      "name": "Patrica Hansen"
+    },
+    {
+      "_id": 9160493,
+      "name": "Juanita Mathis"
+    },
+    {
+      "_id": 7888330,
+      "name": "Elinor Hampton"
+    },
+    {
+      "_id": 4444537,
+      "name": "Velma Barry"
+    },
+    {
+      "_id": 8479477,
+      "name": "Liz Walls"
+    },
+    {
+      "_id": 8108062,
+      "name": "Meghan Cooke"
+    },
+    {
+      "_id": 9635373,
+      "name": "Dawson Dickson"
+    },
+    {
+      "_id": 251990,
+      "name": "Sharp Farmer"
+    },
+    {
+      "_id": 3260028,
+      "name": "Trujillo Gentry"
+    },
+    {
+      "_id": 564810,
+      "name": "Jaime Huber"
+    },
+    {
+      "_id": 8706649,
+      "name": "Evelyn Guerrero"
+    },
+    {
+      "_id": 9184730,
+      "name": "Stanley Roth"
+    },
+    {
+      "_id": 8297160,
+      "name": "Kirby Barber"
+    },
+    {
+      "_id": 5557701,
+      "name": "Gayle Preston"
+    },
+    {
+      "_id": 4818736,
+      "name": "Catherine Dunn"
+    },
+    {
+      "_id": 9565287,
+      "name": "Brooks Banks"
+    },
+    {
+      "_id": 5520023,
+      "name": "Tania Curry"
+    },
+    {
+      "_id": 8912517,
+      "name": "Isabelle Mann"
+    },
+    {
+      "_id": 2276414,
+      "name": "Joy Mcclain"
+    },
+    {
+      "_id": 6282102,
+      "name": "Susana Cunningham"
+    },
+    {
+      "_id": 5605931,
+      "name": "Pitts Valenzuela"
+    },
+    {
+      "_id": 833868,
+      "name": "Ayala Galloway"
+    },
+    {
+      "_id": 2336748,
+      "name": "Walter Harrington"
+    },
+    {
+      "_id": 8124404,
+      "name": "Mejia Lane"
+    },
+    {
+      "_id": 9927069,
+      "name": "Whitfield Mcgowan"
+    },
+    {
+      "_id": 4235865,
+      "name": "Mccarty Booker"
+    },
+    {
+      "_id": 6543471,
+      "name": "Rachael Kramer"
+    },
+    {
+      "_id": 6391032,
+      "name": "Maryann Peck"
+    },
+    {
+      "_id": 7023553,
+      "name": "Charity Sanders"
+    },
+    {
+      "_id": 9228688,
+      "name": "Ferguson Hooper"
+    },
+    {
+      "_id": 6504929,
+      "name": "Cindy Warner"
+    },
+    {
+      "_id": 9084091,
+      "name": "Mercedes Mcfadden"
+    },
+    {
+      "_id": 125829,
+      "name": "Sanford Goff"
+    },
+    {
+      "_id": 5417978,
+      "name": "Rush Stephenson"
+    },
+    {
+      "_id": 8311091,
+      "name": "Jody Silva"
+    },
+    {
+      "_id": 4437844,
+      "name": "Ashlee Sparks"
+    },
+    {
+      "_id": 868861,
+      "name": "Angelina Carrillo"
+    },
+    {
+      "_id": 4617790,
+      "name": "Janis Thomas"
+    },
+    {
+      "_id": 4382461,
+      "name": "Nancy Britt"
+    },
+    {
+      "_id": 9733801,
+      "name": "Lenora Stone"
+    },
+    {
+      "_id": 3859425,
+      "name": "Sophia Walters"
+    },
+    {
+      "_id": 7914898,
+      "name": "Jennie Shepherd"
+    },
+    {
+      "_id": 9591285,
+      "name": "Tucker Terry"
+    },
+    {
+      "_id": 6630891,
+      "name": "Burks Mayer"
+    },
+    {
+      "_id": 5858244,
+      "name": "Watkins Knapp"
+    },
+    {
+      "_id": 1041862,
+      "name": "Lucy King"
+    },
+    {
+      "_id": 5899444,
+      "name": "Janelle Jarvis"
+    },
+    {
+      "_id": 9977022,
+      "name": "Peters Torres"
+    },
+    {
+      "_id": 5420626,
+      "name": "Deana Chambers"
+    },
+    {
+      "_id": 2794223,
+      "name": "Kendra Alston"
+    },
+    {
+      "_id": 5552097,
+      "name": "Huber Moore"
+    },
+    {
+      "_id": 764436,
+      "name": "Zelma Bridges"
+    },
+    {
+      "_id": 6616252,
+      "name": "Tracy Mccarty"
+    },
+    {
+      "_id": 9411340,
+      "name": "Evangelina Clay"
+    },
+    {
+      "_id": 6818292,
+      "name": "Barry Hartman"
+    },
+    {
+      "_id": 5701580,
+      "name": "Melinda Cardenas"
+    },
+    {
+      "_id": 6975548,
+      "name": "Johnson Rodriquez"
+    },
+    {
+      "_id": 9169879,
+      "name": "Beatrice Petersen"
+    },
+    {
+      "_id": 3158480,
+      "name": "Marcy Guy"
+    },
+    {
+      "_id": 1033846,
+      "name": "Weiss Osborne"
+    },
+    {
+      "_id": 4776250,
+      "name": "Mcfadden Graves"
+    },
+    {
+      "_id": 5346822,
+      "name": "Vicky Matthews"
+    },
+    {
+      "_id": 6435674,
+      "name": "Fern Chase"
+    },
+    {
+      "_id": 4100414,
+      "name": "Dunlap Cohen"
+    },
+    {
+      "_id": 6912428,
+      "name": "Bessie Quinn"
+    },
+    {
+      "_id": 161150,
+      "name": "Malone Ingram"
+    },
+    {
+      "_id": 8926624,
+      "name": "Mari Welch"
+    },
+    {
+      "_id": 6689983,
+      "name": "Ruth Zamora"
+    },
+    {
+      "_id": 9505989,
+      "name": "Compton Day"
+    },
+    {
+      "_id": 6610735,
+      "name": "Amanda Martin"
+    },
+    {
+      "_id": 6994423,
+      "name": "Angela Maddox"
+    },
+    {
+      "_id": 6104440,
+      "name": "Santos Norris"
+    },
+    {
+      "_id": 935341,
+      "name": "Cannon Crosby"
+    },
+    {
+      "_id": 664216,
+      "name": "Briggs Mason"
+    },
+    {
+      "_id": 1065037,
+      "name": "Battle Doyle"
+    },
+    {
+      "_id": 7680973,
+      "name": "Joanna Butler"
+    },
+    {
+      "_id": 6526304,
+      "name": "Navarro Blankenship"
+    },
+    {
+      "_id": 5883202,
+      "name": "Mcdaniel Smith"
+    },
+    {
+      "_id": 1652033,
+      "name": "Aida Jacobs"
+    },
+    {
+      "_id": 4429351,
+      "name": "Blackwell Le"
+    },
+    {
+      "_id": 7979539,
+      "name": "Grant Lester"
+    },
+    {
+      "_id": 8467960,
+      "name": "Reed Gardner"
+    },
+    {
+      "_id": 8340769,
+      "name": "Wilda Whitaker"
+    },
+    {
+      "_id": 770916,
+      "name": "Stacie Mckinney"
+    },
+    {
+      "_id": 9518666,
+      "name": "Paige Mcmillan"
+    },
+    {
+      "_id": 7135115,
+      "name": "Gonzalez Davidson"
+    },
+    {
+      "_id": 3563712,
+      "name": "Letha Buck"
+    },
+    {
+      "_id": 3623173,
+      "name": "Jenny Vazquez"
+    },
+    {
+      "_id": 9048035,
+      "name": "Alba Acosta"
+    },
+    {
+      "_id": 2175351,
+      "name": "Sloan Macdonald"
+    },
+    {
+      "_id": 1946138,
+      "name": "Byrd Schwartz"
+    },
+    {
+      "_id": 7737669,
+      "name": "Berta Mendez"
+    },
+    {
+      "_id": 5247707,
+      "name": "Latoya Long"
+    },
+    {
+      "_id": 8983489,
+      "name": "Krystal Maynard"
+    },
+    {
+      "_id": 2421428,
+      "name": "Sherrie Hebert"
+    },
+    {
+      "_id": 1354424,
+      "name": "Chang Hendricks"
+    },
+    {
+      "_id": 130275,
+      "name": "Benita Sharp"
+    },
+    {
+      "_id": 7918949,
+      "name": "Blankenship Shelton"
+    },
+    {
+      "_id": 7844428,
+      "name": "Lakeisha Robertson"
+    },
+    {
+      "_id": 9136760,
+      "name": "Nettie Cantrell"
+    },
+    {
+      "_id": 8212324,
+      "name": "Swanson Barnett"
+    },
+    {
+      "_id": 4113537,
+      "name": "Ramona Rivers"
+    },
+    {
+      "_id": 3588600,
+      "name": "Jimmie Rivas"
+    },
+    {
+      "_id": 3804370,
+      "name": "Susie Dudley"
+    },
+    {
+      "_id": 3353975,
+      "name": "Bobbi Hester"
+    },
+    {
+      "_id": 1174315,
+      "name": "Ada Mitchell"
+    },
+    {
+      "_id": 8578222,
+      "name": "Kidd Murphy"
+    },
+    {
+      "_id": 6445027,
+      "name": "Staci Roy"
+    },
+    {
+      "_id": 1184030,
+      "name": "Gwendolyn Gutierrez"
+    },
+    {
+      "_id": 5098941,
+      "name": "Robbins Aguilar"
+    }
+  ];
+  
+  const list = document.querySelector("#data-list");
+  const prevButton = document.querySelector("#prev");
+  const nextButton = document.querySelector("#next");
+  const pageNumberValue = document.querySelector("#page-number")
+  
+  let startIndex = 0;
+  let endIndex = 10;
+  let pageNumber = 0;
+  
+  pageNumberValue.value = pageNumber
+  
+  const mapData = () => {
+    const slicedData = dataSet
+      .slice(startIndex, endIndex)
+      .map((row) => {
+        return `<li>${row.name}</li>`;
+      })
+      .join("");
+  
+    list.innerHTML = slicedData;
+  }
+  
+  mapData();
+  
+  prevButton.addEventListener("click", () => {
+    if (endIndex < 20) {
+      startIndex = 0;
+      endIndex = 10;
+    } else {
+      startIndex -= 10;
+      endIndex -= 10;
+      pageNumber -= 1;
+    }
+    pageNumberValue.value = pageNumber;
+    mapData();
+  });
+  
+  nextButton.addEventListener("click", () => {
+    if (endIndex < dataSet.length) {
+      startIndex += 10;
+      endIndex += 10;
+      pageNumber += 1;
+    }
+    pageNumberValue.value = pageNumber;
+    mapData();
+  });
+  
+  
+  pageNumberValue.addEventListener("change",(e) => {
+    let currentPageNumber = Number.parseInt(e.target.value)
+    let maxPageNumber = Math.floor(dataSet.length/10)
+    if(currentPageNumber > maxPageNumber){
+     currentPageNumber = maxPageNumber;
+      e.target.value = value
+    }
+    else if(currentPageNumber < 0){
+      currentPageNumber = 0;
+      e.target.value = value
+    }
+     startIndex = currentPageNumber * 10;
+     endIndex = startIndex + 10
+     pageNumber = currentPageNumber
+     mapData();
+  })
